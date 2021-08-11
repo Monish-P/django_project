af0f81626ebe46e62f86b04d62a980a4b34cc61a
Monish-P <121910311046@gitam.in>
2021-08-12 00:05:51 +0530

    commited

--- django_project/urls.py
+++ django_project/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('',include('blog.urls')),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-    path('register/',user_views.register,name='register')
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path('register/',user_views.register,name='register'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
 ]
 
